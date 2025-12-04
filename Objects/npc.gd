8124a79dddbead782fe6df6a7c6e2c974b0bf269
Erin <enguy4@ocdsb.ca>
2025-12-04 18:56:01 -0500

    Day 3!

---
+++ Objects/npc.gd
@@ -0,0 +1,10 @@
+extends Area2D
+
+@onready var interact_name: String = ""
+@onready var is_interactable: bool = true 
+
+var interact: Callable = func():
+	pass
+	
+	
+	
---
+++ UIs/main_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_newgame_pressed() -> void:
+	print("new pressed")
+	var newgamescene = preload("res://Levels/gamescene_1.tscn")
+	get_tree().change_scene_to_packed(newgamescene)
+	
+func _on_settings_pressed() -> void:
+	print("settings pressed")
+	var settingspage = preload("res://UIs/settings.tscn")
+	get_tree().change_scene_to_packed(settingspage)
+	
+
+func _on_quit_game_pressed() -> void:
+	print("quit pressed")
+	get_tree().quit()
+
+
+func _on_load_pressed() -> void:
+	print("load pressed")
