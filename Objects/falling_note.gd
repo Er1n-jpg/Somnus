5743e9bcb8e52567812ac6d3001855e3297a543a
Erin <enguy4@ocdsb.ca>
2025-12-03 20:39:47 -0500

    CRASHOUT + mainscreen

--- Objects/falling_note.gd
+++ Objects/falling_note.gd
@@ -1,8 +1,10 @@
 extends Sprite2D
 
-@export var fall_speed: float = 2.5
+@export var fall_speed: float = 2.0
 
 var init_y_pos: float = -360
+
+var has_passed: bool = false
 
 func _init():
 	set_process(false)
@@ -10,10 +12,15 @@ func _init():
 func _process(delta: float) -> void:
 	global_position += Vector2(0, fall_speed)
 
-	if position.y > 200.0 and $Timer.is_stopped():
+	if global_position.y > 200.0 and $Timer.is_stopped():
 		print($Timer.wait_time - $Timer.time_left)
 		$Timer.stop()
 
-func Setup(target_x: float):
+func Setup(target_x: float, target_frame, int):
 	global_position = Vector2(target_x, init_y_pos)
+	#frame = target_frame
 	set_process(true)
+
+
+func _on_destroyeroftimers_timeout() -> void:
+	queue_free()
--- Objects/keylistener.gd
+++ Objects/keylistener.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 @onready var falling_note = preload("res://Objects/falling_note.tscn")
 @export var key_name: String = ""
 
-
+var falling_key_queue = []
 func _process(delta):
 	if Input.is_action_just_pressed(key_name):
 		Createfallingkey()
@@ -10,4 +10,6 @@ func _process(delta):
 func Createfallingkey():
 	var fn_inst = falling_note.instantiate()
 	get_tree().get_root().call_deferred("add_child",fn_inst)
-	fn_inst.Setup(position.x)
+	fn_inst.Setup(position.x, frame + 4)
+	
+	falling_key_queue.push_back(fn_inst)
